5ab9110839bf3b2adb758f471e56386408284b97
bgomezdh <bgomez@digitalhouse.com>
2022-10-21 15:11:31 -0300

    Add folder day16 about fetch

---
+++ day16-fetch/js/index.js
@@ -0,0 +1,9 @@
+/* API's - Nosotros nos encargaremos de darle estas URL a ustedes*/
+let urlUser = "https://randomuser.me/api/";
+
+/* Seleccionar un elemento del DOM */
+
+
+
+/* Crear fetch */
+
---
+++ day16-fetch/js/provincias.js
@@ -0,0 +1,9 @@
+/* API's */
+let urlProv = "https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre";
+
+/* Seleccionar un elemento del DOM */
+
+
+
+/* Crear fetch */
+
