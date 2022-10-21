dafa56934cf191397c607d77fa4d5ef6edccc0ed
bgomezdh <bgomez@digitalhouse.com>
2022-10-21 16:43:39 -0300

    add folder 15

--- day16-fetch/js/index.js
+++ day16-fetch/js/index.js
@@ -2,8 +2,30 @@
 let urlUser = "https://randomuser.me/api/";
 
 /* Seleccionar un elemento del DOM */
-
+let imagen = document.querySelector(".imagen");
+let sexo = document.querySelector(".sexo");
+let email = document.querySelector(".email");
+let nombre = document.querySelector(".name");
 
 
 /* Crear fetch */
+fetch(urlUser)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    
+    let usuario = data.results[0];
 
+    console.log(usuario);
+
+    imagen.src = usuario.picture.large;
+    sexo.innerText = usuario.gender;
+    email.innerText = usuario.email;
+    nombre.innerText = `${usuario.name.title} ${usuario.name.first} ${usuario.name.last}`;
+
+    return data;
+  })
+  .catch(function (error) {
+    return error;
+  });
--- day16-fetch/js/provincias.js
+++ day16-fetch/js/provincias.js
@@ -1,9 +1,28 @@
 /* API's */
-let urlProv = "https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre";
+let urlProv =
+  "https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre";
 
 /* Seleccionar un elemento del DOM */
-
-
+let listaDOM = document.querySelector(".listaProvincias");
 
 /* Crear fetch */
 
+fetch(urlProv)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    let prov = data.provincias;
+    console.log(prov)
+    let resultado = "";
+    for (let i = 0; i < prov.length; i++) {
+        resultado += `<li><a href="./index.html">${prov[i].nombre} </a></li>`
+        
+    }
+
+    listaDOM.innerHTML = resultado;
+    return data;
+  })
+  .catch(function (error) {
+    return error;
+  });
